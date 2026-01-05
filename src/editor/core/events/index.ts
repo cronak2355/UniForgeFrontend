1499706f79ff0eb8a7bf190e8d382e8db6b6e3f6
Cronak2355 <cronak2355@gmail.com>
2026-01-05 11:39:58 +0900

    fix/merge error fix

--- src/editor/core/events/index.ts
+++ src/editor/core/events/index.ts
@@ -2,7 +2,11 @@ export * from "./EventBus";
 export * from "./ActionRegistry";
 export * from "./ConditionRegistry";
 export * from "./RuleEngine";
+export * from "./SignalStore";
 
 // 사이드 이펙트(등록)를 위해 import
+import "./actions/PresentationActions";
+import "./conditions/EventSignalCondition";
+import "./bootstrap";
 import "./actions/DefaultActions";
 import "./conditions/DefaultConditions";
